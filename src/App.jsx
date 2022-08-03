b156bb9b0c198c03b1768767c7d2ce971caeef81
toshiki-enomoto <107050276+toshiki-enomoto@users.noreply.github.com>
2022-08-04 00:05:30 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+// eslint react-hooks/exhaustive-deps: off
+import React, { useEffect, useState } from "react";
+import { ColofulMessage } from "./components/ColorfulMessage";
+
+const App = () => {
+  console.log("最初");
+
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    // console.log("useEffect!!");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
+  return (
+    <React.Fragment>
+      <h1 style={{ color: "red" }}>こんにちわ</h1>
+      <ColofulMessage color="blue">お元気ですか？</ColofulMessage>
+      <ColofulMessage color="pink">元気です！</ColofulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwichShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( `~`)</p>}
+    </React.Fragment>
+  );
+};
+
+export default App;
